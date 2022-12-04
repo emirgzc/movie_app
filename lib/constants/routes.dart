6c4aa5dbc7e09f6ff79f9f86c78a4d05823473b8
emirgzc <muhammet.gozcu@ogr.dpu.edu.tr>
2022-12-04 15:40:42 +0300

    Routes klasörü eklendi.

---
+++ lib/constants/routes.dart
@@ -0,0 +1,14 @@
+
+/* class Routes {
+  static Route? onGenerateRoute(RouteSettings settings) {
+    late Widget page;
+    switch (settings.name) {
+    }
+
+    return MaterialPageRoute(
+      builder: (context) => page,
+      settings: settings,
+      maintainState: false,
+    );
+  }
+} */
