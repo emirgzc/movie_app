ad586dc808d41a08b5a87ba04721860a2af48356
emirgzc <muhammet.gozcu@ogr.dpu.edu.tr>
2022-12-04 15:42:51 +0300

    Film detay sayfası eklendi.

---
+++ lib/pages/movie_detail_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MovieDetailPage extends StatelessWidget {
+  const MovieDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
