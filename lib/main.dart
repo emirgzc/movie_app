22472dce2d1f2a2d77f799d3a5cc076f6f080604
ismail_kyvsn <ismailkyvsn2000@gmail.com>
2022-12-16 22:47:48 +0300

    sistem ui modu değiştirildi

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:movie_app/pages/movie_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
 
   runApp(const MyApp());
 }
